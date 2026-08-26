c967f4efc02a81ed3a1b03a73e800a4a68e7f84a
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-08-26 14:32:20 -0600

    Add navbar base with logo, links and logout placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router'
 import Layout from './layout/Layout';
-import Main from './home/Home'
+import Home from './home/Home'
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Home />} />
       </Route>
     </Routes>
   )
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,17 @@
 const Navbar = () => {
    return (
-      <header>Navbar</header>
+      <header>
+         <section id="logo">
+            <h2>Prayer Wall</h2>
+         </section>
+         <section id="links">
+            <p>Answered</p>
+            <p>Wall</p>
+         </section>
+         <section id="logout">
+            <p>Logout link</p>
+         </section>
+      </header>
    )
 }
 
