5c66378a7f72cd6b2d946cd0020a891cd6a7e585
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-08-27 14:33:30 -0600

    Import all necessary functions for login and create base of login page

---
+++ src/auth/Login.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { useAuth } from "./AuthContext";
+import { useNavigate, Link } from "react-router";
+
+const Login = () => {
+   const [error, setError] = useState('');
+   const {login} = useAuth();
+   const navigate = useNavigate();
+
+   return (
+      <>
+         <h1>Login</h1>
+      </>
+   )
+}
