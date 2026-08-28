4d68fb0c2beb971a95eef9dbace7fbb34f8e17bb
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-08-28 13:35:46 -0600

    Add api routing for prayers on page load

--- src/prayers/Wall.jsx
+++ src/prayers/Wall.jsx
@@ -1,8 +1,22 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
+import { getPrayers } from "../api/prayers";
+import { useAuth } from "../auth/AuthContext";
 
 const Wall = () => {
-   const [prayers, setPrayers] = useState([])
+   const [prayers, setPrayers] = useState();
+   const {token} = useAuth();
+
+   const syncPrayers = async () => {
+      const retrievedPrayers = await getPrayers(token);
+      setPrayers(retrievedPrayers);
+   }
+   
+   useEffect(() => {
+      syncPrayers();
+   }, [])
+
+   if (!prayers) return <h2>Loading....</h2>
 
    return (
       <>
