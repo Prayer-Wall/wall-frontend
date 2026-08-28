945fa6ff62cf0c2e9925192444e3427221301781
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-08-28 14:51:32 -0600

    Update links position and whether they are hidden with token/active

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,15 +1,24 @@
+import { useAuth } from "../auth/AuthContext";
+import { NavLink, useNavigate } from "react-router";
+
 const Navbar = () => {
+   const {token} = useAuth();
+   const navigate = useNavigate();
+
    return (
       <header>
          <section id="logo">
-            <h2>Prayer Wall</h2>
+            <h2 onClick={() => navigate("/wall")}>Prayer Wall</h2>
          </section>
-         <section id="links">
-            <p>Answered</p>
-            <p>Wall</p>
-         </section>
+         {
+            token ? 
+               <section id="links">
+                  <NavLink to="/" className={({isActive}) => isActive ? "hide" : ""} id="answered">Answered</NavLink>
+                  <NavLink to="/wall" className={({isActive}) => isActive ? "hide" : ""} id="wall-link">Wall</NavLink>
+               </section> : <></>
+         }
          <section id="logout">
-            <p>Logout link</p>
+            {token ? <p>Logout link</p> : <p></p>}
          </section>
       </header>
    )
