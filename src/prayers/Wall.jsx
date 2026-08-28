5fb89715e2c5f09a7c110d554f8905fd4c58d35a
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-08-27 21:45:14 -0600

    Add base of aside and prayers section

--- src/prayers/Wall.jsx
+++ src/prayers/Wall.jsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router";
+
 const Wall = () => {
+   const [prayers, setPrayers] = useState([])
+
    return (
-      <section>
-         <h1>Wall</h1>
-      </section>
+      <>
+         <aside>
+            <p>Add Prayer</p>
+            <Link to="/">Answered</Link>
+         </aside>
+         <section>
+            <h1>Wall</h1>
+         </section>
+      </>
    )
 }
 
