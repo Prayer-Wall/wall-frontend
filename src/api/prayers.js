4bf3754d7e7baee34c79f966c102020d3bb9f121
Jeremiah Torbit <jeremiah.torbit@gmail.com>
2026-08-29 22:30:14 -0600

    Add add prayer route and function to add prayer component

--- src/api/prayers.js
+++ src/api/prayers.js
@@ -8,3 +8,17 @@ export const getPrayers = async (token) => {
    const prayers = await response.json();
    return prayers;
 }
+
+export const addPrayer = async (token, prayer) => {
+   const response = await fetch(API + "/prayers/add", {
+      method: "POST",
+      headers: {Authorization: "Bearer " + token},
+      body: JSON.stringify(prayer)
+   });
+
+   const result = await response.json();
+   if (!response.ok) {
+      throw new Error(response.message)
+   }
+   return result;
+}
--- src/prayers/AddPrayer.jsx
+++ src/prayers/AddPrayer.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+import { addPrayer } from "../api/prayers";
+
 const AddPrayer = () => {
+   const [error, setError] = useState();
+   const {token} = useAuth();
+
+   const tryAddPrayer = async (formData) => {
+      try {
+         setError(null)
+   
+         const prayer = formData.get("prayer");
+
+         await addPrayer(token, prayer)
+      } catch (e) {
+         setError(e.message)
+      }
+
+
+   }
+
    return (
-      <h1>Add Prayer</h1>
+      <form id="prayer-form" action={tryAddPrayer}>
+         <textarea type="text" name="prayer" required />
+         <button>Add</button>
+      </form>
    )
 }
 
